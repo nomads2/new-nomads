945ed9d8502490c499771128e42071185f6bccd9
TravisThatcher <recompas@gmail.com>
2014-10-29 17:10:46 -0400

    Starting to make client scripts more object oriented

--- public/js/client-script.js
+++ public/js/client-script.js
@@ -2,6 +2,9 @@
  * Nomads Basic Mobile and Desktop Client
  */
 
+// Global Namespace
+var NomadsClient = NomadsClient || {};
+
 var socket;
 
 var localUser = {
@@ -21,7 +24,7 @@ var messageToSend = {
 $(document).ready(function() {   
   socket = io.connect();
 
-  geoLocate();
+  NomadsClient.geoLocate();
 
   //Show Login Div
   $('#namefield').focus();
@@ -71,7 +74,7 @@ $(document).ready(function() {
 
 });
 
-var geoLocate = function(){
+NomadsClient.geoLocate = function(){
   if(geoPosition.init()){  // Geolocation Initialisation
               geoPosition.getCurrentPosition(success_callback,error_callback,{enableHighAccuracy:true});
   }else{
@@ -84,6 +87,13 @@ var geoLocate = function(){
     localUser.latitude = parseFloat(p.coords.latitude);
     localUser.longitude = parseFloat(p.coords.longitude);
     console.log("Lat/Long = " + localUser.latitude + " " + localUser.longitude);
+
+    // Send User Long/Lat
+    var messageToSend = new Object();
+    messageToSend.id = localUser.id;
+    messageToSend.username = localUser.username;
+    messageToSend.timestamp = new Date();
+    socket.emit('userGeo', messageToSend);
   }
 
   function error_callback(p){
