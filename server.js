f9a86834cc79a5a131b8899a929d8bc6bca3b373
TravisThatcher <recompas@gmail.com>
2018-04-02 18:04:17 -0400

    updated server and client for new versions of libs

--- server.js
+++ server.js
@@ -1,59 +1,39 @@
 //setup Dependencies
-var connect = require('connect')
-    , express = require('express')
-    , io = require('socket.io')
-    , port = (process.env.PORT || 8081);
-
-//var oscFunctions = require('./osc-bundle.js'); //require
+var connect = require('connect');
 
 //Setup Express
-var server = express();
-/*
-server.configure(function(){
-    server.set('views', __dirname + '/views');
-    server.set('view options', { layout: false });
-    server.use(connect.bodyParser());
-    server.use(express.cookieParser());
-    server.use(express.session({ secret: "shhhhhhhhh!"}));
-    server.use(connect.static(__dirname + '/public'));
-    server.use(server.router);
-});
+var express = require('express');
+var path = require('path');
+let app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-//setup the errors
-server.error(function(err, req, res, next){
-    if (err instanceof NotFound) {
-        res.render('404.jade', { locals: { 
-                  title : '404 - Nomads aren\'t here'
-                 ,description: ''
-                 ,author: ''
-                 ,analyticssiteid: 'XXXXXXX' 
-                },status: 404 });
-    } else {
-        res.render('500.jade', { locals: { 
-                  title : 'The Server Encountered an Error'
-                 ,description: ''
-                 ,author: ''
-                 ,analyticssiteid: 'XXXXXXX'
-                 ,error: err 
-                },status: 500 });
-    }
-});
-*/
+var port = (process.env.PORT || 8081);
+
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.set('env', 'development');
+
+
+
 server.listen(port);
 
 //Setup OSC functions
-var oscMessage = require("./osc-bundle.js");
+//var oscMessage = require("./osc-bundle.js");
 
 //Setup Socket.IO
-var io = io.listen(server);
-io.sockets.on('connection', function(socket){
+
+io.on('connection', function(socket){
   console.log('Client Connected');
 
   //when new user enters his/her name, display.
   socket.on('newuser', function (data) {
     console.log('new user added! ' + data.username);
-    //console.log(data);
-    oscMessage.sendOSC('/newuser', data);
+    console.log(data);
+    //oscMessage.sendOSC('/newuser', data);
     socket.broadcast.emit('user_confirmed', data);
   });
 
@@ -61,7 +41,7 @@ io.sockets.on('connection', function(socket){
   socket.on('message', function(data){
     socket.broadcast.emit('proc_update',data); //send data to all clients for processing sketch
     socket.broadcast.emit('client_update',data); //send data back to all clients?
-    oscMessage.sendOSC('/object', data);  //just send a single block instead of multiple, smaller OSC messages
+    //oscMessage.sendOSC('/object', data);  //just send a single block instead of multiple, smaller OSC messages
     // sendOSCText('/thought', data);
     // sendOSC('/geolocation', [ data.latitude, data.longitude ] );
     //socket.emit('server_message',data); // send data back to individual client?
@@ -82,8 +62,8 @@ io.sockets.on('connection', function(socket){
 
 /////// ADD ALL YOUR ROUTES HERE  /////////
 
-server.get('/', function(req,res){
-  res.render('client.jade', {
+app.get('/', function(req,res){
+  res.render('client.pug', {
     locals : { 
               title : 'Nomads'
              ,description: 'Your Page Description'
@@ -93,8 +73,8 @@ server.get('/', function(req,res){
   });
 });
 
-server.get('/client', function(req,res){
-  res.render('client.jade', {
+app.get('/client', function(req,res){
+  res.render('client.pug', {
     locals : { 
               title : 'Nomads'
              ,description: 'Nomads main client'
@@ -104,8 +84,8 @@ server.get('/client', function(req,res){
   });
 });
 
-server.get('/display', function(req,res){
-  res.render('display.jade', {
+app.get('/display', function(req,res){
+  res.render('display.pug', {
     locals : { 
               title : 'Nomads'
              ,description: 'Nomads message disply'
@@ -115,8 +95,8 @@ server.get('/display', function(req,res){
   });
 });
 
-server.get('/conference', function(req,res){
-  res.render('conference.jade', {
+app.get('/conference', function(req,res){
+  res.render('conference.pug', {
     locals : { 
               title : 'Nomads'
              ,description: 'Nomads conference display'
@@ -126,16 +106,25 @@ server.get('/conference', function(req,res){
   });
 });
 
-
-//A Route for Creating a 500 Error (Useful to keep around)
-server.get('/500', function(req, res){
-    throw new Error('This is a 500 Error');
+/*
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-//The 404 Route (ALWAYS Keep this as the last route)
-server.get('/*', function(req, res){
-    throw new NotFound('Sorry, that page is not on this server');
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('404');
 });
+*/
 
 function NotFound(msg){
     this.name = 'NotFound';
